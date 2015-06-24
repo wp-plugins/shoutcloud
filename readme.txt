714b982126400496bf38e7f16d5dd3252dea863c
xmeltrut <xmeltrut@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-24 05:55:34 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/shoutcloud/trunk@1186625 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Plugin Name ===
+Contributors: xmeltrut
+Donate link: http://example.com/
+Tags: shoutcloud, shout, shouting, microservices
+Requires at least: 4.0.0
+Tested up to: 4.2.2
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+INTEGRATES YOUR BLOG WITH SHOUTCLOUD. EVERYTHING IS BETTER WHEN IT IS SHOUTED!
+
+== Description ==
+
+SHOUTCLOUD IS A MICROSERVICE THAT TRANSFORMS YOUR STRINGS TO UPPERCASE. THIS PLUGIN INTEGRATES ALL OF YOUR CONTENT
+WITH THE SERVICE - ALL TITLES, CONTENT AND TAGS WILL BE CONVERTED.
+
+ADVANTAGES:
+
+* MICROSERVICES ARE THE FUTURE
+* REDUCE SERVER LOAD BY DOING YOUR STRING MANIPULATION IN THE CLOUD
+* BUILD ANTICIPATION BY DRASTICALLY INCREASING LOAD TIME
+
+DISTADVANTAGES:
+
+* NONE, THE CLOUD IS ALWAYS BETTER!
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/` directory
+2. Enjoy!
+
+== Screenshots ==
+
+1. Blog homepage
+
+== Frequently Asked Questions ==
+
+= What about images and links? Will this break them? =
+
+You may be concerned that shouting URLs would break the case-sensitive links. Don't be! The plugin uses a smart filtering
+system that removes all images and hyperlinks so that your audience can concentrate on the text.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release
---
+++ shoutcloud.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: SHOUTCLOUD
+Plugin URI:  http://github.com/xmeltrut/ShoutCloudWordPressPlugin
+Description: INTEGRATE SHOULDCLOUD WITH WORDPRESS. EVERYTHING IS BETTER WHEN YOU SHOUT IT.
+Version:     1.0
+Author:      Chris Worfolk
+Author URI:  http://www.worfolk.co.uk/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+function shoutcloud_prepare ($str) {
+    return strip_tags($str, '<p><br><br /><div><pre><blockquote><em><strong>');
+}
+
+function shoutcloud_request ($str) {
+    $jsonInput = [
+        'INPUT' => $str
+    ];
+    
+    $ch = curl_init();
+    
+    if ($ch) {
+        curl_setopt($ch, CURLOPT_URL, 'HTTP://API.SHOUTCLOUD.IO/V1/SHOUT');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsonInput));
+        $result = curl_exec($ch);
+        curl_close($ch);
+        
+        if ($result) {
+            $json = json_decode($result);
+            return $json->OUTPUT;
+        }
+    }
+    
+    return null;
+}
+
+function shoutcast_string ($str) {
+    $input  = shoutcloud_prepare($str);
+    $output = shoutcloud_request($input);
+    
+    if ($output === null) {
+        $output = strtoupper($input);
+    }
+    
+    return $output;
+}
+
+add_filter('the_title', 'shoutcast_string');
+add_filter('the_content', 'shoutcast_string');
+add_filter('the_tags', 'shoutcast_string');
